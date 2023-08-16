be46c286d41c1b53ea5bb5dc0e5ccf96a6cae313
mingming <jimin23623@gmail.com>
2023-08-16 12:07:39 +0900

    키보드 마우스 처리

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -26,6 +26,32 @@ export class Keyboard {
         document.addEventListener("keydown", this.#onKeyDown.bind(this))
         document.addEventListener("keyup", this.#onKeyUp.bind(this))
         this.#inputEl.addEventListener("input", this.#onInput)
+        this.#keyboardEl.addEventListener("mousedown", this.#onMouseDown)
+        // 키보다 밖에서 마우스를 뗄 수도 있고 다양한 변수가 있기 때문에 document에 걸었음
+        document.addEventListener("mouseup", this.#onMouseUp.bind(this))
+    }
+
+    #onMouseUp(event) {
+        const keyEl = event.target.closest("div.key");
+        // !! 느낌표 두개를 붙여줘서 확실하게 boolean 값이 출력될 수 있도록 처리
+        const isActive = !!keyEl?.classList.contains("active")
+        // data-set 속성 값을 가져와 저장
+        const val = keyEl?.dataset.val;
+        if (isActive && !!val && val !== "Space" && val !== "Backspace") {
+            this.#inputEl.value += val;
+        }
+        if (isActive && val === "Space") {
+            this.#inputEl.value += " ";
+        }
+        if (isActive && val === "Backspace") {
+            this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+        }
+        // 안에서 this를 썼다면 위 addeventlistener에서 bind(this) 필수
+        this.#keyboardEl.querySelector(".active")?.classList.remove("active");
+    }
+
+    #onMouseDown(event) {
+        event.target.closest("div.key")?.classList.add("active");
     }
 
     #onInput(event) {
